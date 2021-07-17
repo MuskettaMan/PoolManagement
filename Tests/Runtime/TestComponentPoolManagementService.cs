5bc69a7dec0852f5a95bc906fa81c8a10ae72ee3
Ferri de Lange <ev.fdl.ve@gmail.com>
2021-07-17 14:10:35 +0200

    Written tests for `ComponentPoolManagementService`.

---
+++ Tests/Runtime/TestComponentPoolManagementService.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using NSubstitute;
+
+public class TestComponentPoolManagementService : MonoBehaviour
+{
+	public TestableComponentObjectPoolBehaviour componentObjectPoolBehaviour;
+
+	[SetUp]
+	public void SetUp()
+	{
+		componentObjectPoolBehaviour = new GameObject().AddComponent<TestableComponentObjectPoolBehaviour>();
+	}
+
+	[TearDown]
+	public void TearDown()
+	{
+		Destroy(componentObjectPoolBehaviour.gameObject);
+	}
+
+	[Test]
+	public void ObjectCreated_WhenRequestingEmptyPool_GameObjectPoolManagementServiceCreatedIsCalled()
+	{
+		var @object = componentObjectPoolBehaviour.RequestObject();
+
+		componentObjectPoolBehaviour.GameObjectPoolManagementService.Received().ObjectCreated(@object.gameObject);
+	}
+
+	[Test]
+	public void ObjectDestroyed_WhenDestroyingPool_GameObjectPoolManagementServiceDestroyedIsCalled()
+	{
+		var @object = componentObjectPoolBehaviour.RequestObject();
+		componentObjectPoolBehaviour.ReturnObject(@object);
+		Destroy(componentObjectPoolBehaviour);
+
+		componentObjectPoolBehaviour.GameObjectPoolManagementService.Received().ObjectDestroyed(@object.gameObject);
+	}
+
+	[Test]
+	public void ObjectRequested_WhenRequestingEmptyPool_GameObjectPoolManagementServiceRequestedIsCalled()
+	{
+		var @object = componentObjectPoolBehaviour.RequestObject();
+
+		componentObjectPoolBehaviour.GameObjectPoolManagementService.Received().ObjectRequested(@object.gameObject);
+	}
+
+	[Test]
+	public void ObjectReturned_WhenReturningToPool_GameObjectPoolManagementServiceReturnedIsCalled()
+	{
+		var @object = componentObjectPoolBehaviour.RequestObject();
+		componentObjectPoolBehaviour.ReturnObject(@object);
+
+		componentObjectPoolBehaviour.GameObjectPoolManagementService.Received().ObjectReturned(@object.gameObject);
+	}
+}
---
+++ Tests/Runtime/Testable Components/TestableComponentObjectPoolBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using NSubstitute;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestableComponentObjectPoolBehaviour : ComponentObjectPoolBehaviour<Transform>
+{
+	public GameObjectPoolManagementService GameObjectPoolManagementService { get; private set; } = Substitute.For<GameObjectPoolManagementService>(true);
+	public ComponentPoolManagementService<Transform> ComponentPoolManagementService { get; private set; }
+
+	protected override void Awake()
+	{
+		prefab = new GameObject().transform;
+		ComponentPoolManagementService = new ComponentPoolManagementService<Transform>(GameObjectPoolManagementService);
+		base.Awake();
+	}
+
+	protected override IPoolManagementService<Transform> InitializePoolManagementService() => ComponentPoolManagementService;
+}
